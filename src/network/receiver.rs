02fd4ee62bd2f742557a747afb703fc3bc1c5147
xsware <xsware@outlook.com>
2026-08-12 10:54:20 +0200

    receive loop now spawns its own thread

--- src/network/receiver.rs
+++ src/network/receiver.rs
@@ -1,10 +1,6 @@
 use std::{
     path::Path,
-    sync::{
-        Arc, Mutex,
-        atomic::{AtomicBool, Ordering},
-    },
-    thread::{self, JoinHandle},
+    sync::{Arc, Mutex},
 };
 
 use displaydoc::Display;
@@ -32,15 +28,14 @@ pub enum Error {
 #[allow(unused)]
 pub enum ReceiveLoopStatus {
     NotStarted,
-    Receiving(JoinHandle<()>),
+    Receiving,
     ShuttingDown,
 }
 
 pub struct Receiver {
     connection_control: Arc<ConnectionControl>,
     error_log: Arc<ErrorLog>,
-    status: Mutex<ReceiveLoopStatus>,
-    stop: Arc<AtomicBool>,
+    receive_loop: Mutex<Option<ReceiveLoop>>,
 }
 
 impl Receiver {
@@ -48,14 +43,12 @@ impl Receiver {
         Self {
             connection_control,
             error_log,
-            status: Mutex::new(ReceiveLoopStatus::NotStarted),
-            stop: Arc::default(),
+            receive_loop: Mutex::new(None),
         }
     }
 
     pub fn start_receiving(&self, receive_dir: &Path) -> Result<(), Error> {
-        let mut status = self.status.lock().unwrap();
-        match *status {
+        match self.receive_status() {
             ReceiveLoopStatus::NotStarted => {}
             _ => return Err(Error::AlreadyReceiving),
         }
@@ -69,31 +62,52 @@ impl Receiver {
             .ok_or(Error::NoConnection)?
             .try_clone()?;
 
-        let mut event = ReceiveLoop::new(connection, self.stop.clone());
         let error_log = self.error_log.clone();
         let receive_dir = receive_dir.to_path_buf();
-        let join_handle = thread::spawn(move || {
-            let _subscription = event.subscribe(Box::new(move |packet| {
-                match FileTransmission::write_file_from_packet_data(&packet, &receive_dir) {
-                    Ok(file_path) => error_log.log(format!(
-                        "received file: \"{}\"",
-                        file_path.to_str().unwrap()
-                    )),
-                    Err(error) => {
-                        error_log.log(format!("error during receive: {}", error));
-                        return;
-                    }
+        let packet_handler =
+            move |packet: &Vec<u8>| match FileTransmission::write_file_from_packet_data(
+                &packet,
+                &receive_dir,
+            ) {
+                Ok(file_path) => error_log.log(format!(
+                    "received file: \"{}\"",
+                    file_path.to_str().unwrap()
+                )),
+                Err(error) => {
+                    error_log.log(format!("error during receive: {}", error));
+                    return;
                 }
-            }));
-            event.start();
-        });
+            };
 
-        *status = ReceiveLoopStatus::Receiving(join_handle);
+        *self.receive_loop.lock().unwrap() = Some(ReceiveLoop::start(
+            connection,
+            vec![Box::new(packet_handler)],
+        ));
 
         Ok(())
     }
 
+    pub fn receive_status(&self) -> ReceiveLoopStatus {
+        let receive_loop_guard = self.receive_loop.lock().unwrap();
+        if let Some(receive_loop) = &*receive_loop_guard {
+            if receive_loop.stopped() {
+                ReceiveLoopStatus::ShuttingDown
+            } else {
+                ReceiveLoopStatus::Receiving
+            }
+        } else {
+            ReceiveLoopStatus::NotStarted
+        }
+    }
+
     pub fn stop(&self) {
-        self.stop.store(true, Ordering::Relaxed);
+        let mut receive_loop_guard = self.receive_loop.lock().unwrap();
+        let Some(receive_loop) = &*receive_loop_guard else {
+            self.error_log
+                .log("receiving has not yet started: unable to stop".to_string());
+            return;
+        };
+        receive_loop.stop();
+        *receive_loop_guard = None;
     }
 }
